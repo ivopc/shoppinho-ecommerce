4f86994745cc09b37d5ffaaaed53bf5c8bb4d71b
Ivopc <ivoopc@gmail.com>
2024-02-04 16:03:04 -0300

    first app made with flutter ok

--- client/flutter_dart/lib/main.dart
+++ client/flutter_dart/lib/main.dart
@@ -4,6 +4,161 @@ void main() {
   runApp(const MyApp());
 }
 
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+  final String name;
+  final String location;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: 
+                    Text(
+                      name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold
+                      )
+                    )),
+                    Text(
+                      location,
+                      style: TextStyle(
+                        color: Colors.grey[500]
+                      )
+                    )
+              ],
+            )
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500]
+          ),
+          const Text("41")
+        ],
+      )
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: "CALL",
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: "ROUTE",
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: "SHARE",
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color
+            )
+          )
+        )
+      ],
+    );
+  }
+
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      )
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({
+    super.key,
+    required this.image
+  });
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image, 
+      width: 600, 
+      height: 240, 
+      fit: BoxFit.cover
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,7 +172,25 @@ class MyApp extends StatelessWidget {
         title: const Text(appTitle)
       ),
         body: const Center(
-          child: Text("Hello World"),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ImageSection(
+                  image: "images/lake.png"
+                ),
+                TitleSection(
+                  name: "Ivopc",
+                  location: "São Paulo, SP",
+                ),
+                ButtonSection(),
+                TextSection(
+                  description: 
+                    "Text in the pubspec.yaml respects whitespace and text case. Write the changes to the file as given in the previous example."
+                    "This change might require you to restart the running program to display the image."
+                )
+              ]
+            ),
+          )
         )
       )
     );
